227d385985336d9a21348b036bd38afea5bacc8b
Kai <kai.hollberg@googlemail.com>
2015-03-05 17:07:29 +0100

    toying around...

--- functions.php
+++ functions.php
@@ -1,13 +1,6 @@
 <?php
-function filter_images($content){
-    // '<div class="box-flex-image">\1\2\3</div>'
-    return preg_replace_callback('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', function($match){
-        if(strpos($match[0],'large-image') !== false){
-            return 'BLA';
-        } else {
-            return '<div class="box-flex-image">'.$match[0].'</div>';
-        }
-    }, $content);
+function filter_p_images($content){
+    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '<div class="box-flex-image">\1\2\3</div>', $content);
 }
 
-add_filter('the_content', 'filter_images');
+add_filter('the_content', 'filter_p_images');
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <title><?php wp_title(); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/normalize.css" type="text/css" media="all" />
+    <link href='http://fonts.googleapis.com/css?family=Fira+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
