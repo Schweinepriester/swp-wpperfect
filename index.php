bad8a7997676d687eedb3306e62431e83a8befd2
Kai <kai.hollberg@googlemail.com>
2015-03-05 13:51:21 +0100

    learing stuff :)

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php get_header(); ?>
 
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php
+                    the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+                ?>
+            </header>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+            <footer class="entry-footer">
+                
+            </footer>
+        </article>
+    <?php
+    endwhile;
+else :
+    // TODO
+endif;
+?>
+
 <?php get_footer(); ?>
