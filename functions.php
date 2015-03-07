b4ff2c9bccb1fa7cf3cf40b28af14d4eb9d75b00
Kai <kai.hollberg@googlemail.com>
2015-03-07 13:31:36 +0100

    wordpress image suffix sucks...

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ function remove_width_height($html){
 
 function swp_theme_setup(){
     add_image_size( 'extra-large', 1920, 1920);
+    // add_image_size( 'extra-large-lowres', 1920, 1920);
 }
 
 function my_custom_sizes( $sizes ) {
@@ -18,8 +19,48 @@ function my_custom_sizes( $sizes ) {
     ) );
 }
 
+function ad_update_jpeg_quality($meta_id, $attach_id, $meta_key, $attach_meta) {
+
+    if ($meta_key == '_wp_attachment_metadata') {
+
+        $post = get_post($attach_id);
+
+        if ($post->post_mime_type == 'image/jpeg' && is_array($attach_meta['sizes'])) {
+
+            $pathinfo = pathinfo($attach_meta['file']);
+            $uploads = wp_upload_dir();
+            $dir = $uploads['basedir'] . '/' . $pathinfo['dirname'];
+
+            foreach ($attach_meta['sizes'] as $size => $value) {
+
+                $image = $dir . '/' . $value['file'];
+                $resource = imagecreatefromjpeg($image);
+
+                if ($size == 'extra-large-lowres') {
+                    // set the jpeg quality for 'spalsh' size
+                    imagejpeg($resource, $image, 10);
+                } elseif ($size == 'spalsh1') {
+                    // set the jpeg quality for the 'splash1' size
+                    // imagejpeg($resource, $image, 30);
+                } else {
+                    // set the jpeg quality for the rest of sizes
+                    // imagejpeg($resource, $image, 100);
+                    continue;
+                }
+
+                // or you can skip a paticular image size
+                // and set the quality for the rest:
+                // if ($size == 'splash') continue;
+
+                imagedestroy($resource);
+            }
+        }
+    }
+}
+
 add_filter('the_content', 'filter_p_images');
 // add_filter('image_send_to_editor', 'remove_width_height');
 add_filter( 'jpeg_quality', create_function( '', 'return 100;' ) );
 add_action( 'after_setup_theme', 'swp_theme_setup' );
 add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+// add_action('added_post_meta', 'ad_update_jpeg_quality');
