3449d21f0f8a2dda99d4978ef2c9fb4e0561f189
Kai <kai.hollberg@googlemail.com>
2015-03-07 04:10:28 +0100

    new image size

--- functions.php
+++ functions.php
@@ -8,5 +8,18 @@ function remove_width_height($html){
     return $html;
 }
 
+function swp_theme_setup(){
+    add_image_size( 'extra-large', 1920, 1920);
+}
+
+function my_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'extra-large' => __( 'Extra Large' ),
+    ) );
+}
+
 add_filter('the_content', 'filter_p_images');
 // add_filter('image_send_to_editor', 'remove_width_height');
+add_filter( 'jpeg_quality', create_function( '', 'return 100;' ) );
+add_action( 'after_setup_theme', 'swp_theme_setup' );
+add_filter( 'image_size_names_choose', 'my_custom_sizes' );
