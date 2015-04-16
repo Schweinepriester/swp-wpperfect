a876b03f0ccfa8708e66f08e7150375d4887428a
Kai <kai.hollberg@googlemail.com>
2015-04-16 16:07:50 +0200

    first really dirty gallery

--- functions.php
+++ functions.php
@@ -74,6 +74,9 @@ function swp_modify_images($content){
     return preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $document->saveHTML()));
 }
 
+if ( ! isset( $content_width ) )
+    $content_width = 1024;
+
 function swp_theme_setup(){
     update_option( 'thumbnail_size_w', 300 );
     update_option( 'thumbnail_size_h', 300 );
@@ -182,4 +185,66 @@ function swp_uploadprogressive($image_data){
     $image_editor = wp_get_image_editor($image_data['file']);
     $saved_image = $image_editor->save($image_data['file']);
     return $image_data;
+}
+
+// from http://stackoverflow.com/questions/19802157/change-wordpress-default-gallery-output
+add_filter('post_gallery', 'my_post_gallery', 10, 2);
+function my_post_gallery($output, $attr) {
+    global $post;
+
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby'])
+            unset($attr['orderby']);
+    }
+
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => 'dl',
+        'icontag' => 'dt',
+        'captiontag' => 'dd',
+        'columns' => 3,
+        'size' => 'thumbnail',
+        'include' => '',
+        'exclude' => ''
+    ), $attr));
+
+    $id = intval($id);
+    if ('RAND' == $order) $orderby = 'none';
+
+    if (!empty($include)) {
+        $include = preg_replace('/[^0-9,]+/', '', $include);
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    }
+
+    if (empty($attachments)) return '';
+
+    // Here's your actual output, you may customize it to your need
+    $output = "<div class=\"slideshow-wrapper\">\n";
+    // $output .= "<ul data-orbit>\n";
+
+    // Now you loop through each attachment
+    foreach ($attachments as $id => $attachment) {
+        // Fetch the thumbnail (or full image, it's up to you)
+//      $img = wp_get_attachment_image_src($id, 'medium');
+//      $img = wp_get_attachment_image_src($id, 'my-custom-image-size');
+        $img = wp_get_attachment_image_src($id, 'large');
+
+        $output .= "<a>\n";
+        $output .= tevkori_extend_image_tag("<img src=\"{$img[0]}\" width=\"{$img[1]}\" height=\"{$img[2]}\" alt=\"\" />\n", $id, null, null, null, null, 'large', null);
+        $output .= "</a>\n";
+    }
+
+    // $output .= "</ul>\n";
+    $output .= '<div style="clear: both"></div>';
+    $output .= "</div>\n";
+
+    return $output;
 }
