ebbe58a442386c19423b5659aaa44ba31d308499
Kai <kai.hollberg@googlemail.com>
2015-04-27 12:04:20 +0200

    js

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
         <footer>
             <?php wp_footer(); ?>
-            <script src="<?php echo get_template_directory_uri (); ?>/js/iconic.min.js"></script>
         </footer>
     </body>
 </html>
