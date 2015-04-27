2c274c5c663c2636e16035df96403f3914d5754a
Kai <kai.hollberg@googlemail.com>
2015-04-27 11:49:15 +0200

    cleanup

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
         <footer>
             <?php wp_footer(); ?>
+            <script src="<?php echo get_template_directory_uri (); ?>/js/iconic.min.js"></script>
         </footer>
     </body>
 </html>
