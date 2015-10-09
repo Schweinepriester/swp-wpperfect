cffa9a294d34e4d1775bb6d3eb48d303a506d4d5
Kai <kai.hollberg@googlemail.com>
2015-10-09 10:22:35 +0200

    move from google fonts to mozilla CDN
    see https://github.com/mozilla/Fira

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <title><?php wp_title(); ?></title>
         <!-- <link rel="profile" href="http://gmpg.org/xfn/11" /> !? -->
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/normalize.css" type="text/css" media="all" />
-        <link href='//fonts.googleapis.com/css?family=Fira+Sans|Lato:700|Lobster+Two:700italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css" type='text/css'>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
