fd8aea799112e1436071f936165cce7dd4f810f3
Kai <kai.hollberg@googlemail.com>
2015-10-12 12:31:11 +0200

    removed some old stuff

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-add_theme_support( 'automatic-feed-links' ); // activate wordpress feeds
-// add_theme_support('soil-relative-urls');
+// activate wordpress feeds
+add_theme_support( 'automatic-feed-links' );
 
 // use the modified image editor!
 add_filter( 'wp_image_editors', 'swp_image_editors');
@@ -13,9 +13,6 @@ add_filter( 'the_content', 'filter_p_images' );
 // add_filter( 'the_content', 'swp_modify_images' );
 add_action( 'after_setup_theme', 'swp_theme_setup' );
 add_action( 'send_headers', 'swp_strict_transport_security' );
-
-// legacy
-// add_filter( 'image_size_names_choose', 'my_custom_sizes' );
 
 // from http://antsanchez.com/remove-new-wordpress-emoji-support/
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -35,7 +32,7 @@ function filter_p_images($content){
     return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '<div class="box-flex-image">\1\2\3</div>', $content);
 }
 
-function swp_modify_images($content){
+function ($content){
     $document = new DOMDocument();
     $document->loadHTML($content);
     $images = $document->getElementsByTagName('img');
