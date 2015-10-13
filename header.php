eb7f5d5fd917da1ae7baad071489a7b07c0346a6
Kai <kai.hollberg@googlemail.com>
2015-10-13 14:16:45 +0200

    no prefetch

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
         <title><?php wp_title(); ?></title>
         <!-- <link rel="profile" href="http://gmpg.org/xfn/11" /> !? -->
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/normalize.css" type="text/css" media="all" />
-        <link rel="prefetch" href="//code.cdn.mozilla.net/fonts/fira.css">
-        <link rel="prefetch" href='//fonts.googleapis.com/css?family=Lato:700|Lobster+Two:700italic' type='text/css'>
+        <link rel="stylesheet" href="//code.cdn.mozilla.net/fonts/fira.css">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:700|Lobster+Two:700italic" type="text/css">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
