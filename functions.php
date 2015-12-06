67863a8b13eba6daf9aa22f58a1b468e91465d81
Kai <kai.hollberg@googlemail.com>
2015-12-06 03:22:28 +0100

    trailingslashit

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function swp_strict_transport_security() {
 }
 
 function swp_hpkp(){
-    include ABSPATH.'/../swp_wp_extra/swp_hpkp.php';
+    include trailingslashit(ABSPATH).'/../swp_wp_extra/swp_hpkp.php';
 }
 
 function filter_p_images($content){
