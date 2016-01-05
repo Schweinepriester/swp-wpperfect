a6225de9e58a39d28db0d19dd63fd9bf19c84cfe
Kai <kai.hollberg@googlemail.com>
2016-01-06 00:22:30 +0100

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -9,29 +9,18 @@ gulp.task('watch', function(){
     gulp.watch(paths.css, ['css']);
 });
 
-gulp.task('less', function () {
-    var less = require('gulp-less');
-    var path = require('path');
-
-    return gulp.src('./dev/*.less')
-        .pipe(less({
-            paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
-        .pipe(gulp.dest('./'));
-});
-
 gulp.task('css', function () {
     var postcss = require('gulp-postcss');
     var sourcemaps = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer-core');
-    var csswring = require('csswring');
+    var autoprefixer = require('autoprefixer');
     var customProperties = require("postcss-custom-properties");
     var colorFunction = require("postcss-color-function");
     var selector = require('postcss-custom-selectors');
+    var nested = require('postcss-nested');
 
     var processors = [
+        nested,
         autoprefixer({browsers: ['last 2 version']}),
-        csswring,
         customProperties(),
         colorFunction(),
         selector()
