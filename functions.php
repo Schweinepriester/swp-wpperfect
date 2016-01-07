a76cf33b450eb78c4826b7f426e90ef9898d6287
Kai <Schweinepriester@users.noreply.github.com>
2016-01-07 14:25:23 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ function swp_hpkp(){
 }
 
 function filter_p_images($content){
-    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '<div class="box-flex-image">\1\2\3</div>', $content);
+    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '<figure class="box-flex-image">\1\2<figcaption>\3</figcaption></figure>', $content);
 }
 
 function swp_modify_images($content){
