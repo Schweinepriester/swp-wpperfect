ff5ddac8ed1aee28031c1fd0700a2e5b84f23785
Kai <Schweinepriester@users.noreply.github.com>
2016-05-31 13:21:51 +0200

    remove '//', HTTPS only now

--- header.php
+++ header.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <!-- <link rel="profile" href="http://gmpg.org/xfn/11" /> !? -->
+        <link rel="dns-prefetch" href="https://code.cdn.mozilla.net">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/normalize.css" type="text/css" media="all" />
-        <link rel="stylesheet" href="//code.cdn.mozilla.net/fonts/fira.css">
-        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:700|Lobster+Two:700italic">
+        <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:700|Lobster+Two:700italic">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
