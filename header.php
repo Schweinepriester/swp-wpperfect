79d34a337a28ef4c9a2de2dbae42625db7f1ac44
Schweinepriester <Schweinepriester@users.noreply.github.com>
2018-02-17 22:27:20 +0100

    actually update normalize; add support for server side syntax highlighting; system font as pre-swap global font

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/fonts/fira/fira.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:700|Lobster+Two:700i">
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/dist/monokai.min.css">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
--- notes.md
+++ notes.md
@@ -14,6 +14,10 @@
 - Link: <https://wordpress.org/plugins/ewww-image-optimizer/>
 - Why: better jpegs
 
+#### WP-Pygments
+
+- <https://github.com/WellingGuzman/WP-Pygments>
+
 #### StripTease
 
 - Link: <https://wordpress.org/plugins/striptease/>
