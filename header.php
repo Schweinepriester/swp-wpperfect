ecf1523195ed3ff87537bc5b5869cc1fee29f016
Kai <Schweinepriester@users.noreply.github.com>
2021-04-15 17:27:50 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="profile" href="http://gmpg.org/xfn/11" /> !? -->
         <link rel="preconnect" href="<?php echo get_site_url(); ?>">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/dist/normalize.min.css" type="text/css" media="all" />
@@ -10,7 +11,6 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo get_template_directory_uri (); ?>/css/dist/monokai.min.css">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
     </head>
